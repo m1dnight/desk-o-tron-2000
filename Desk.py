f2558da2b4905298e4492d21c6f400860e55dcd5
Christophe De Troyer <christophe.detroyer@gmail.com>
2022-04-19 14:58:01 +0200

    update

--- Desk.py
+++ Desk.py
@@ -2,7 +2,7 @@ import asyncio
 import struct
 from asyncio import Task
 from typing import Any, Callable, Union
-
+import logging
 import bleak.exc
 from bleak import BleakClient
 
@@ -59,11 +59,14 @@ class Desk:
     # Desk Control
 
     async def get_desk_height(self):
+        logging.debug('Retrieving desk height')
         try:
             result = await self.__desk_connection.read_gatt_char(self.uuid_height)
             parsed = Desk.__unpack_data(result)
             return parsed
         except bleak.exc.BleakError as e:
+            logging.warning('Error retrieving desk height')
+            logging.warning(e)
             return 0, 0
 
     async def move_up(self):
@@ -73,18 +76,22 @@ class Desk:
             pass
 
     async def move_down(self):
+        logging.debug('Moving desk up 1 unit.')
         try:
             await self.__desk_connection.write_gatt_char(self.uuid_cmd, self.cmd_down)
         except bleak.exc.BleakError as e:
+            logging.warning('Error moving desk up 1 unit.')
+            logging.warning(e)
             pass
 
     async def move_to(self, target: float) -> None:
-        print("Moving to ")
-        print(target)
+        logging.debug("Moving desk to {}".format(target))
         start_height, start_speed = await self.get_desk_height()
+        logging.debug("Currently at {}".format(start_height))
 
         # The desk moves in centimers, so makes no sense to try and hit milimeter precision.
         if abs(target - start_height) <= 20:
+            logging.debug("Desk is close enough, not moving")
             return None
 
         direction = self.cmd_down if target < start_height else self.cmd_up
@@ -96,10 +103,12 @@ class Desk:
         # Otherwise, schedule a task every 6 updates.
         # 6 is a magic number.
         update_counter = 0
+        retries = 0
 
         def _move(current_height_mm: float, speed_mm_s: float):
             nonlocal update_counter
             nonlocal direction
+            nonlocal retries
             update_counter = update_counter + 1
 
             # If the desk moved (speed was not 0.0) it must be
@@ -107,6 +116,7 @@ class Desk:
             # If it's not at the right height yet, only send another command
             # if the desk sent around 6 updates.
             if abs(current_height_mm - target) <= 20:
+                logging.debug("Desk moved within threshold: {}".format(abs(current_height_mm - target)))
                 asyncio.create_task(self.remove_callback(self.uuid_height, _move))
                 move_completed.set_result(True)
 
@@ -115,8 +125,15 @@ class Desk:
             #  2. The desk is being pushed on, so it can't go up enough, so we have to stop.
             # Stopping means resolving the future so move_to_target can return.
             elif speed_mm_s == 0:
-                asyncio.create_task(self.remove_callback(self.uuid_height, _move))
-                move_completed.set_result(True)
+                if retries >= 1:
+                    logging.debug("Desk stopped moving, but did not reach target.")
+                    asyncio.create_task(self.remove_callback(self.uuid_height, _move))
+                    move_completed.set_result(True)
+                else:
+                    logging.debug("Desk stopped moving, trying again!")
+                    await asyncio.sleep(2)
+                    retries = retries + 1
+                    asyncio.create_task(self.move_down())
 
             elif update_counter % 6 == 0:
                 if direction == self.cmd_up:
@@ -134,33 +151,35 @@ class Desk:
     # Connection Monitor
 
     async def __attempt_connect(self):
+        logging.debug("Attempting connection to desk")
         try:
             await self.__desk_connection.connect()
         except Exception as e:
+            logging.debug("Attempt failed.")
             return False
         return True
 
     async def __reconnect(self):
+        logging.debug("Attempting to reconnect to desk")
         self.__desk_connection = BleakClient(self.mac_address)
         try:
             while not self.__desk_connection.is_connected:
-                print("Attempting reconnect..")
                 if await self.__attempt_connect():
-                    print("Connection established")
+                    logging.debug("Connection established")
                     return True
                 else:
-                    print("Attempt to connect failed, waiting for 30 seconds..")
+                    logging.debug("Attempt to connect failed, waiting for 30 seconds..")
                     await asyncio.sleep(30)
 
             await self.__initialize_monitor()
             await self.get_desk_height()
         except Exception as e:
-            print("An error occurred reconnecting to the desk: {}".format(e))
+            logging.error("An error occurred reconnecting to the desk: {}".format(e))
             return False
 
     async def __monitor_connection(self):
         if not self.__desk_connection.is_connected:
-            print("Desk disconnected")
+            logging.debug("Monitor detected desk is disconnected")
             await self.__reconnect()
         else:
             await self.get_desk_height()
@@ -186,6 +205,7 @@ class Desk:
     async def __initialize_callbacks(self):
         def __height_changed(_sender: int, data: bytearray):
             current_height_mm, speed_mm_s = self.__unpack_data(data)
+            logging.debug("Height changed: height: {}, speed: {}".format(current_height_mm, speed_mm_s))
             callbacks = self.__callbacks[self.uuid_height]
 
             for cb in callbacks:
@@ -197,7 +217,7 @@ class Desk:
     async def __install_debug_callback(self):
         def _cb(height, speed):
             if speed != 0:
-                print("Height: {}, speed: {}".format(height, speed))
+                logging.debug("Height: {}, speed: {}".format(height, speed))
 
         self.install_callback(self.uuid_height, _cb)
 
